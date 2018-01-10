c2b4a43242304cf698a94a66cad074b734eeea46
项小盆友 <1259653875@qq.com>
2018-01-10 15:51:09 +0800

    新建项目

---
+++ InHeart-Training/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InHeart-Training
+//
+//  Created by 项小盆友 on 2018/1/10.
+//  Copyright © 2018年 项小盆友. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+target 'InHeart-Training' do
+pod 'AFNetworking'
+pod 'YYModel'
+pod 'Masonry'
+end 
+
